3d4a722893f1cea21387671fe7d548ae9d0327e3
standupright <grudnov.dev@gmail.com>
2024-09-14 19:12:50 +0500

    FEAT: BREAKING CHANGES REMOVE EXPRESS

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -3,6 +3,4 @@ import { config } from 'dotenv';
 config();
 
 export const TOKEN = process.env.TOKEN || '';
-export const CONFIRMATION = process.env.CONFIRMATION || '';
 export const OPEN_API_KEY= process.env.OPEN_API_KEY || '';
-export const PORT = process.env.APP_PORT || '';
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,9 @@
 import VkBot from 'node-vk-bot-api';
 import { core, openAI } from './core';
-import { CONFIRMATION, PORT, TOKEN } from 'constants/env';
-import express from 'express';
-import bodyParser from 'body-parser';
+import { TOKEN } from 'constants/env';
 
-const app = express();
 const bot = new VkBot({
   token: TOKEN,
-  confirmation: CONFIRMATION,
 });
 
 bot.on(async (ctx) => {
@@ -31,15 +27,8 @@ bot.command('/clear', async (ctx) => {
 }});
 
 
-// bot.startPolling();
+bot.startPolling();
 
-app.use(bodyParser.json());
-
-// @ts-ignore-next-line
-app.post('/', bot.webhookCallback);
-
-
-app.listen(PORT || 3000);
 
 process.once('SIGINT', () => bot.stop())
 process.once('SIGTERM', () => bot.stop())
